96b9c65b79721fe0ef5b82211ea70352ef616ed1
FornaxA <fornaxa@servitising.org>
2018-11-07 15:18:32 +0100

    Consensus: Refactor: Decouple CValidationState from main::AbortNode()
    Based on https://github.com/bitcoin/bitcoin/pull/5768/commits/49fd398fa9d1ec4aee9ad7feb55838c7c82d575b
    
    Separates validation functions into consensus/validation.h

--- src/main.cpp
+++ src/main.cpp
@@ -3176,15 +3176,15 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     }
 
     // Flush spend/mint info to disk
-    if (!zerocoinDB->WriteCoinSpendBatch(vSpends)) return state.Abort(("Failed to record coin serials to database"));
-    if (!zerocoinDB->WriteCoinMintBatch(vMints)) return state.Abort(("Failed to record new mints to database"));
+    if (!zerocoinDB->WriteCoinSpendBatch(vSpends)) return AbortNode(state, ("Failed to record coin serials to database"));
+    if (!zerocoinDB->WriteCoinMintBatch(vMints)) return AbortNode(state, ("Failed to record new mints to database"));
 
     //Record accumulator checksums
     DatabaseChecksums(mapAccumulators);
 
     if (fTxIndex)
         if (!pblocktree->WriteTxIndex(vPos))
-            return state.Abort("Failed to write transaction index");
+            return AbortNode(state, "Failed to write transaction index");
 
     // add this block to the view's block chain
     view.SetBestBlock(pindex->GetBlockHash());
