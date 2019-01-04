06d51170592e26f02e16b16f99026b7726565b56
cevap <dev@i2pmail.org>
2019-01-04 10:30:43 +0100

    Update copyrights: bitcoin

--- src/test/test_ion.cpp
+++ src/test/test_ion.cpp
@@ -1,6 +1,7 @@
-// Copyright (c) 2011-2015 The Bitcoin Core developers
-// Copyright (c) 2018-2020 The Ion Core developers
-// Distributed under the MIT software license, see the accompanying
+// Copyright (c) 2011-2019 The Bitcoin Core developers
+// Copyright (c) 2017 The PIVX developers
+// Copyright (c) 2018 The Ion developers
+// Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "test_ion.h"
