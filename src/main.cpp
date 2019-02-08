c58d2e9b127134cf2ff6ad97de14078fe24f902e
furszy <5377650+furszy@users.noreply.github.com>
2019-02-08 15:32:49 +0100

    AcceptBlock() check for double spent serials only on main chain flag.
    Signed-off-by: cevap <dev@i2pmail.org>

--- src/main.cpp
+++ src/main.cpp
@@ -4720,12 +4720,14 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                 }
             }
         } else {
-            for (CTxIn xIonInput : xIONInputs) {
-                    CoinSpend spend = TxInToZerocoinSpend(xIonInput);
-                    if (!ContextualCheckZerocoinSpend(stakeTxIn, spend, pindex, 0))
-                        return state.DoS(100,error("%s: ContextualCheckZerocoinSpend failed for tx %s", __func__,
-                                stakeTxIn.GetHash().GetHex()), REJECT_INVALID, "bad-txns-invalid-xion");
-            }
+            if(!isBlockFromFork)
+                for (CTxIn xIonInput : xIONInputs) {
+                        CoinSpend spend = TxInToZerocoinSpend(xIonInput);
+                        if (!ContextualCheckZerocoinSpend(stakeTxIn, spend, pindex, 0))
+                            return state.DoS(100,error("%s: main chain ContextualCheckZerocoinSpend failed for tx %s", __func__,
+                                    stakeTxIn.GetHash().GetHex()), REJECT_INVALID, "bad-txns-invalid-xion");
+                }
+
         }
 
     }
