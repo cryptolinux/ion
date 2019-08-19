9b9ea281bb73ea8614c7006156f144eda9944544
FornaxA <fornaxa@servitising.org>
2019-08-19 11:44:48 +0200

    Add helper functions for token distribution

--- src/rpc/blockchain.h
+++ src/rpc/blockchain.h
@@ -1,37 +1,18 @@
-// Copyright (c) 2017 The Bitcoin Core developers
+// Copyright (c) 2017-2018 The ION Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_RPC_BLOCKCHAIN_H
-#define BITCOIN_RPC_BLOCKCHAIN_H
+#ifndef RPC_BLOCKCHAIN_H
+#define RPC_BLOCKCHAIN_H
 
-class CBlock;
-class CBlockIndex;
-class UniValue;
+#include "amount.h"
+#include "consensus/tokengroups.h"
+#include "script/standard.h"
 
-/**
- * Get the difficulty of the net wrt to the given block index, or the chain tip if
- * not provided.
- *
- * @return A floating point number that is a multiple of the main net minimum
- * difficulty (4295032833 hashes).
- */
-double GetDifficulty(const CBlockIndex* blockindex = nullptr);
+#include <unordered_map>
+#include <vector>
 
-/** Callback for when block tip changed. */
-void RPCNotifyBlockChange(bool ibd, const CBlockIndex *);
-
-/** Block description to JSON */
-UniValue blockToJSON(const CBlock& block, const CBlockIndex* blockindex, bool txDetails = false);
-
-/** Mempool information to JSON */
-UniValue mempoolInfoToJSON();
-
-/** Mempool to JSON */
-UniValue mempoolToJSON(bool fVerbose = false);
-
-/** Block header to JSON */
-UniValue blockheaderToJSON(const CBlockIndex* blockindex);
+/** Used to get a list of token owners to pay  */
+void GetChainTokenBalances(std::unordered_map<std::string, CAmount>& mAtomBalances, CAmount& nAtomCount, const CTokenGroupID& needle);
 
 #endif
-
