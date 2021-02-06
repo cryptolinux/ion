bytzck/BYTZ

Let regtest have its own qt settings (#3286)
Also, avoid messing up testnet settings while playing in regtest

src/qt/guiconstants.h
@@ -36,5 +36,6 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_APP_NAME_DEFAULT "Dash-Qt"
 #define QAPP_APP_NAME_TESTNET "Dash-Qt-testnet"
 #define QAPP_APP_NAME_DEVNET "Dash-Qt-%s"
+#define QAPP_APP_NAME_REGTEST "Dash-Qt-regtest"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H

cryptolinux/ion

Signed-off-by: cevap <dev@i2pmail.org>

src/qt/guiconstants.h
@@ -36,5 +36,6 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_APP_NAME_DEFAULT "Ion-Qt"
 #define QAPP_APP_NAME_TESTNET "Ion-Qt-testnet"
 #define QAPP_APP_NAME_DEVNET "Ion-Qt-%s"
+#define QAPP_APP_NAME_REGTEST "Ion-Qt-regtest"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H