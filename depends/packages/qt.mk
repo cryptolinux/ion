wagerr/wagerr

Update to build s390x and powerpc64le in gitian build This will be necessary to update snapcraft.yaml to use the release binaries to build snap packages on snapcraft store

depends/packages/qt.mk
@@ -107,9 +107,9 @@ $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
 $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 $(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform wagerr-linux-g++
-$(package)_config_opts_s390x_linux += -platform linux-g++ -xplatform linux-g++-64
+$(package)_config_opts_s390x_linux += -platform linux-g++ -xplatform wagerr-linux-g++
 $(package)_config_opts_powerpc_linux += -platform linux-g++ -xplatform linux-g++-32
-$(package)_config_opts_powerpc64le_linux += -platform linux-g++ -xplatform linux-g++-64
+$(package)_config_opts_powerpc64le_linux += -platform linux-g++ -xplatform wagerr-linux-g++
 $(package)_config_opts_sparc64_linux += -platform linux-g++ -xplatform linux-g++-64
 $(package)_config_opts_alpha_linux += -platform linux-g++ -xplatform linux-g++-64
 $(package)_config_opts_m68k_linux += -platform linux-g++ -xplatform linux-g++-32

cryptolinux/ion

Signed-off-by: cevap <dev@i2pmail.org>

depends/packages/qt.mk
@@ -112,9 +112,9 @@ $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_x86_64_linux = -xplatform linux-g++-64
 $(package)_config_opts_aarch64_linux = -xplatform linux-aarch64-gnu-g++
 $(package)_config_opts_riscv64_linux = -platform linux-g++ -xplatform ion-linux-g++
-$(package)_config_opts_s390x_linux += -platform linux-g++ -xplatform linux-g++-64
+$(package)_config_opts_s390x_linux += -platform linux-g++ -xplatform ion-g++
 $(package)_config_opts_powerpc_linux += -platform linux-g++ -xplatform linux-g++-32
-$(package)_config_opts_powerpc64le_linux += -platform linux-g++ -xplatform linux-g++-64
+$(package)_config_opts_powerpc64le_linux += -platform linux-g++ -xplatform ion-linux-g++
 $(package)_config_opts_sparc64_linux += -platform linux-g++ -xplatform linux-g++-64
 $(package)_config_opts_alpha_linux += -platform linux-g++ -xplatform linux-g++-64
 $(package)_config_opts_m68k_linux += -platform linux-g++ -xplatform linux-g++-32