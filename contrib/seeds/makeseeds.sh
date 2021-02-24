73f83b02f128a427020b82d1d6eee06292ecd247
cevap <dev@i2pmail.org>
2021-02-24 05:16:44 +0100

    contrib - update make seeds scripts and remove nodes_main/test.txt

--- contrib/seeds/makeseeds.sh
+++ contrib/seeds/makeseeds.sh
@@ -20,36 +20,37 @@
 #         and check if every IP address has a port in nodes_main.txt and nodes_test.txt
 #   thats it.
 
-MAINSEEDS="37.247.55.128:12700
-163.172.188.223:12700
-37.120.155.10:12700
+MAINSEEDS="37.120.155.2:12700
+80.194.62.218:12700
 88.202.231.139:12700
-72.17.88.56:12700
-149.29.2.18:12700
-109.123.118.38:12700
-155.138.225.26:12700"
+95.216.29.84:12700
+98.203.82.233:12700
+104.207.149.116:12700
+142.93.233.62:12700
+144.76.97.45:12700
+144.202.93.126:12700
+178.63.235.201:12700
+[2a01:4f9:2b:60b::2]:12700
+[2a02:c207:2026:3409:6763::7]:12700"
 
-MAINONIONSEEDS="mdbpyigvanhrv6jg.onion:12700
-ndqgxcnvvd2s6pbr.onion:12700
-3szeblrpwucr6eze.onion:12700
+MAINONIONSEEDS="3szeblrpwucr6eze.onion:12700
+ajjhrmc7ungkr4l2.onion:12700
+g2x6xrzpef6fktez.onion:12700
+heaoykp3ic2ykdb4.onion:12700
 mdbpyigvanhrv6jg.onion:12700
-g2x6xrzpef6fktez.onion:12700"
+ndqgxcnvvd2s6pbr.onion:12700
+sp6khtufo2zc4zql.onion:12700"
 
-TESTNETSEEDS="37.247.55.128:27170
-163.172.188.223:27170
-37.120.155.10:27170
-88.202.231.139:27170
-72.17.88.56:27170
-149.29.2.18:27170
-109.123.118.38:27170
-155.138.225.26:27170"
+TESTNETSEEDS="37.120.155.2:27170
+88.202.231.139:27170"
 
-TESTNETONIONSEEDS="mdbpyigvanhrv6jg.onion:27170
-ndqgxcnvvd2s6pbr.onion:27170
-3szeblrpwucr6eze.onion:27170
-mdbpyigvanhrv6jg.onion:27170
+TESTNETONIONSEEDS="3szeblrpwucr6eze.onion:27170
+ajjhrmc7ungkr4l2.onion:27170
 g2x6xrzpef6fktez.onion:27170
-heaoykp3ic2ykdb4.onion:27170"
+heaoykp3ic2ykdb4.onion:27170
+mdbpyigvanhrv6jg.onion:27170
+ndqgxcnvvd2s6pbr.onion:27170
+sp6khtufo2zc4zql.onion:27170"
 
 setcustomseeds () {
 cat <<EOF | tee -a $1
