51014b05ef834686fdc2465cfec559160d79c5e6
cevap <dev@i2pmail.org>
2021-03-01 01:46:36 +0100

    update hardcoded seeds

--- contrib/seeds/makeseeds.sh
+++ contrib/seeds/makeseeds.sh
@@ -21,36 +21,30 @@
 #   thats it.
 
 MAINSEEDS="37.120.155.2:12700
-80.194.62.218:12700
-88.202.231.139:12700
-95.216.29.84:12700
-98.203.82.233:12700
-104.207.149.116:12700
-142.93.233.62:12700
-144.76.97.45:12700
-144.202.93.126:12700
-178.63.235.201:12700
-[2a01:4f9:2b:60b::2]:12700
-[2a02:c207:2026:3409:6763::7]:12700"
+88.202.231.139:12700"
 
-MAINONIONSEEDS="3szeblrpwucr6eze.onion:12700
+MAINONIONSEEDS="xp57z6folwlbjltj.onion:12700
+zj6p2uhmjvlyqkkm.onion:12700
 ajjhrmc7ungkr4l2.onion:12700
-g2x6xrzpef6fktez.onion:12700
-heaoykp3ic2ykdb4.onion:12700
-mdbpyigvanhrv6jg.onion:12700
-ndqgxcnvvd2s6pbr.onion:12700
-sp6khtufo2zc4zql.onion:12700"
+sgt4peuocandnm3d.onion:12700
+ffvhe3jqgzlgcz3i.onion:12700
+rlohaqpm42nh7z53.onion:12700
+fgdp4io6eapqkpe2.onion:12700
+5jnxtpa2pnkgbnme.onion:12700"
 
 TESTNETSEEDS="37.120.155.2:27170
 88.202.231.139:27170"
 
 TESTNETONIONSEEDS="3szeblrpwucr6eze.onion:27170
-ajjhrmc7ungkr4l2.onion:27170
-g2x6xrzpef6fktez.onion:27170
-heaoykp3ic2ykdb4.onion:27170
-mdbpyigvanhrv6jg.onion:27170
-ndqgxcnvvd2s6pbr.onion:27170
-sp6khtufo2zc4zql.onion:27170"
+xp57z6folwlbjltj.onion:27170
+bglmeozjd2av473l.onion:27170
+sgt4peuocandnm3d.onion:27170
+yhenkcie237wy2hd.onion:27170
+443nzgqiitpyibs4.onion:27170
+o5a24enfkxpcguxt.onion:27170
+fgdp4io6eapqkpe2.onion:27170
+iqix73pdf3gyj3yb.onion:27170
+ajjhrmc7ungkr4l2.onion:27170"
 
 setcustomseeds () {
 cat <<EOF | tee -a $1
