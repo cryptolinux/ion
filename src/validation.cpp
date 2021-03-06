09b461e4a0495748839b14c45477b49dc24c25cc
cevap <dev@i2pmail.org>
2021-03-06 06:49:19 +0100

    fix validation failure - Keys don't match

--- src/validation.cpp
+++ src/validation.cpp
@@ -273,7 +273,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "DarkCoin Signed Message:\n";
+const std::string strMessageMagic = "Ion Signed Message:\n";
 
 // Internal stuff
 namespace {
